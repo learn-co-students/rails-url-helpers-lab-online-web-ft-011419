0498e3e6736240d96b63cbaa57635214da150483
Sim Greenbaum <gbs4ever2@gmail.com>
2019-03-19 20:46:58 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+  def actvie
+    @student = Student.find(params[:id])
+  render 'views/students/show.html.erb'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :students
+
+  get '/students/:id/activate' , to: 'students#active',  as: 'activate_student'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
